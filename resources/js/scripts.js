dd4d782b21402f82d3809ba1d67f323ac3a4aed9
stevesperti <42934471+stevesperti@users.noreply.github.com>
2018-11-13 15:08:19 +1100

    Navigation js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,2 +1,22 @@
 cssVars();
 
+$('.nav-prompt').click(function () {
+
+if ($('.nav-inner').attr('aria-expanded') == 'false'){
+    
+    $('.nav-inner').attr('aria-expanded', 'true');
+
+    $('.nav-prompt').text('Close Navigation');
+
+} else {
+
+    $('.nav-inner').attr('aria-expanded', 'false');
+
+    $('.nav-prompt').text('Open Navigation');
+}
+
+
+
+
+
+});
